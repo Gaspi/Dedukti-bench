0831b646cbeb39e80bc5b51ff2e162bddbe79ec4
Gaspard Ferey <gaspard.ferey@inria.fr>
2018-04-19 17:30:34 +0200

    First version 0.1

--- bench.sh
+++ bench.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-sep='|'
-default_output_file=results.csv
-sources_folder=dedukti
+script_version=0.1
 
+sep='|'
+database=~/bench.csv
+sources_folder=dedukti
+benchs_folder=benchs
 
 uuid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-date=$(date)
+date_unix_timestamp=$(date +%s)
+date=$(date -d @$date_unix_timestamp)
 
 echo "-------------------------------------------------------------"
 echo "                 Dedukti Benchmarking Tool"
@@ -16,15 +19,21 @@ echo "    Date:     $date"
 echo "    Revision: $1"
 echo "-------------------------------------------------------------"
 
+if [ ! -f $database ]
+then
+	bash fresh-db.sh "$database"
+fi
 
-output="${output:-$default_output_file}"
 dir=$(pwd)
-out_file=$dir/$output
+out_file=$dir/out.csv
+BENCHS=$dir/$benchs_folder/
 SOURCES=$dir/$sources_folder/
-DKCHECK=$SOURCES/dkcheck.native
+
+rm -rf $BENCHS $SOURCES
+mkdir $BENCHS
 
 timec=/usr/bin/time
-time_format=" $sep %E $sep %U $sep %S $sep %M $sep %K $sep %W"
+time_format=" $sep %e $sep %U $sep %S $sep %M $sep %K $sep %W $sep %x"
 export TIME="$time_format"
 
 printf "$uuid" > $out_file
@@ -33,30 +42,107 @@ log () {
 	printf " $sep $1" >> $out_file
 }
 
-log "$date"
+log_file () {
+	cat $1 | tr -d "\n" >> $out_file
+}
+
+log "$script_version"
+log "$date_unix_timestamp"
 log "$1"
 
-rm -rf $SOURCES
 git clone https://github.com/Deducteam/Dedukti.git $SOURCES
 cd $SOURCES
 git checkout $1
 
-hashrev=$(git rev-parse HEAD)
+commit_hash=$(git rev-parse HEAD)
+commit_date=$(git --no-pager log -1 --format=%ct)
 
-log "$hashrev"
-
-$timec -a -o $out_file make
-
+log "$commit_hash"
+log "$commit_date"
 
 
-# Run tests here
+echo "-------------------------------------------------------------"
+echo "                    Bench 0 : Compilation"
+echo "-------------------------------------------------------------"
+$timec -a -o $SOURCES/time.csv make
+
+log_file $SOURCES/time.csv
+
+export DKCHECK=$SOURCES/dkcheck.native
+export DKDEP=$SOURCES/dkdep.native
 
 
 
 
 
 echo "-------------------------------------------------------------"
-echo "                    Benchmarks done !"
+echo "                    Bench 1 : Matita"
 echo "-------------------------------------------------------------"
-echo "    Results are saved in: $out_file"
+mkdir $BENCHS/bench1
+cd $BENCHS/bench1
+
+wget -q https://deducteam.github.io/data/libraries/matita.tar.gz
+tar zxf matita.tar.gz
+rm matita/matita_arithmetics_factorial.dk
+rm matita/matita_arithmetics_binomial.dk
+rm matita/matita_arithmetics_chebyshev_*.dk
+rm matita/matita_arithmetics_chinese_reminder.dk
+rm matita/matita_arithmetics_congruence.dk
+rm matita/matita_arithmetics_fermat_little_theorem.dk
+rm matita/matita_arithmetics_gcd.dk
+rm matita/matita_arithmetics_ord.dk
+rm matita/matita_arithmetics_primes.dk
+
+$timec -a -o $BENCHS/bench1/time.csv make -C matita
+log_file $BENCHS/bench1/time.csv
+
+
+
+echo "-------------------------------------------------------------"
+echo "                    Bench 2 : DKlib"
+echo "-------------------------------------------------------------"
+mkdir $BENCHS/bench2
+cd $BENCHS/bench2
+
+git clone -q -b v2.6 https://github.com/rafoo/dklib.git
+
+$timec -a -o $BENCHS/bench2/time.csv make -C dklib
+log_file $BENCHS/bench2/time.csv
+
+
+
+
+
+
+# --------- Run further tests here -------
+
+
+
+
+
+
+echo "-------------------------------------------------------------"
+echo "                   Benchmarks are done !"
+echo "-------------------------------------------------------------"
+
+nb_lines=$(wc -l <$out_file)
+
+if [ $nb_lines -eq 0 ]
+then	
+	# Removing conflicting lines in databases (probaly useless
+	#sed -i '/^$uuid/ d' $database
+	
+	# Exporting results to database
+	cat $out_file >> $database
+	echo "" >> $database
+	
+	echo -e "    \033[0;32mSUCCESS !\033[0m"
+	echo "    Results are saved in: $out_file"
+	echo "    Database was updated: $database"
+else
+	echo -e "    \033[0;31mFAILURE !\033[0m"
+	echo "    Output file has more than 1 line."
+	echo "    Something probably went wrong..."
+fi
+
 echo "-------------------------------------------------------------"
---
+++ fresh-db.sh
@@ -0,0 +1,35 @@
+
+db=$1
+
+
+echo "-------------------------------------------------------------"
+echo "                 Creating fresh database"
+echo "-------------------------------------------------------------"
+
+printf "bench_id" > $db
+
+pr () {
+	printf " | $1" >> $db
+}
+
+pr "bench_version"
+pr "bench_timestamp"
+pr "bench_argument"
+pr "commit_hash"
+pr "commit_timestamp"
+
+pr_times () {
+	pr "bench$1_real_time"
+	pr "bench$1_user_time"
+	pr "bench$1_kernel_time"
+	pr "bench$1_max_memory"
+	pr "bench$1_mean_memory"
+	pr "bench$1_swaps"
+	pr "bench$1_status"
+}
+
+pr_times 0
+pr_times 1
+pr_times 2
+
+echo "" >> $db
