1371691dbad405a1c2c150f16f396bf6d70807ba
Gaspard Ferey <gaspard.ferey@inria.fr>
2018-04-20 10:40:34 +0200

    Modified factorial test. Check for unexpected output.

--- bench.sh
+++ bench.sh
@@ -54,6 +54,7 @@ log "$commit_hash"
 log "$commit_date"
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                    Bench 0 : Compilation"
 echo "-------------------------------------------------------------"
@@ -61,6 +62,7 @@ $timec -a -o $SOURCES/compile.csv make
 log_file $SOURCES/compile.csv
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                    Bench 1 : Tests"
 echo "-------------------------------------------------------------"
@@ -74,6 +76,7 @@ export DKDEP=$SOURCES/dkdep.native
 
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                    Bench 2 : Matita"
 echo "-------------------------------------------------------------"
@@ -92,6 +95,7 @@ log_file $BENCHS/matita/time.csv
 
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                    Bench 3 : Matita / Factorial"
 echo "-------------------------------------------------------------"
@@ -99,11 +103,12 @@ echo "-------------------------------------------------------------"
 tar zxf matita.tar.gz matita/matita_arithmetics_factorial.dk
 rm matita/matita_arithmetics_factorial.dko
 
-$timec -a -o $BENCHS/matita/time_fact.csv make -C matita
+$timec -a -o $BENCHS/matita/time_fact.csv make -C matita matita_arithmetics_factorial.dko
 log_file $BENCHS/matita/time_fact.csv
 
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                    Bench 4 : Examples"
 echo "-------------------------------------------------------------"
@@ -118,6 +123,7 @@ export DKDEP=$SOURCES/dkdep.native
 
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                    Bench 5 : DKlib"
 echo "-------------------------------------------------------------"
@@ -134,13 +140,14 @@ log_file $BENCHS/dklib/time.csv
 
 
 
-# --------- Run further tests here -------
+# --------- Run more benches here -------
 
 
 
 
 
 
+echo ""
 echo "-------------------------------------------------------------"
 echo "                   Benchmarks are done !"
 echo "-------------------------------------------------------------"
@@ -148,17 +155,24 @@ echo "-------------------------------------------------------------"
 nb_lines=$(wc -l <$out_file)
 
 if [ $nb_lines -eq 0 ]
-then	
-	# Removing conflicting lines in databases (probaly useless
-	#sed -i '/^$uuid/ d' $database
-	
-	# Exporting results to database
-	cat $out_file >> $database
-	echo "" >> $database
-	
-	echo -e "    \033[0;32mSUCCESS !\033[0m"
-	echo "    Results are saved in: $out_file"
-	echo "    Database was updated: $database"
+then
+	if grep "exited with non-zero" $out_file ;
+	then
+		echo -e "    \033[0;31mFAILURE !\033[0m"
+		echo "    Some bench exited with non-zero return code."
+		echo "    Something probably went wrong..."
+	else
+		# Removing conflicting lines in databases (probaly useless
+		#sed -i '/^$uuid/ d' $database
+		
+		# Exporting results to database
+		cat $out_file >> $database
+		echo "" >> $database
+		
+		echo -e "    \033[0;32mSUCCESS !\033[0m"
+		echo "    Results are saved in: $out_file"
+		echo "    Database was updated: $database"
+	fi
 else
 	echo -e "    \033[0;31mFAILURE !\033[0m"
 	echo "    Output file has more than 1 line."
